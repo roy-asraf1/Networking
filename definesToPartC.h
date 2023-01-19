c298e49a3279f068e341766a85da89594363add1
Roy Asraf <117677549+roy-asraf1@users.noreply.github.com>
2023-01-20 01:30:01 +0200

    Update definesToPartC.h

--- definesToPartC.h
+++ definesToPartC.h
@@ -1,48 +1,46 @@
-#include <stdio.h>
-#include <netinet/ip.h>
-#include <net/ethernet.h>
 #include <pcap.h>
-#include <arpa/inet.h>
-#include <unistd.h>
-#include <string.h>
 #include <stdio.h>
+#include <string.h>
 #include <stdlib.h>
-#include <netinet/tcp.h>
-#include <net/ethernet.h>
-#include <sys/socket.h>
+#include <ctype.h>
 #include <errno.h>
-#include <time.h>
-#include <pcap/pcap.h>
-#include <netinet/ip_icmp.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <net/ethernet.h>
+#include <netinet/ip.h>
+#include <unistd.h>
+#include <netinet/tcp.h>
 
-struct icmpheader
-{
-    unsigned char type;
-    unsigned char code;
-    unsigned short int checksum;
-    unsigned short int id;
-    unsigned short int seq;
-};
+int seq =0;
+typedef __u_char u_char;
 
+/* default snap length (maximum bytes per packet to capture) */
+#define SNAP_LEN 1518
+/* IP Header */
 struct ipheader
 {
-    unsigned char ip_ihl : 4;
-    unsigned char ip_ver : 4;
-    unsigned char ip_tos;
-    unsigned short int ip_len;
-    unsigned short int ip_id;
-    unsigned short int ip_flag : 3;
-    unsigned short int ip_offset : 13;
-    unsigned char ip_ttl;
-    unsigned char ip_protocol;
-    unsigned short int ip_checksum;
-    struct in_addr source_ip;
-    struct in_addr dest_ip;
+    unsigned char       iph_ihl : 4,    // IP header length
+                        iph_ver : 4;    // IP version
+    unsigned char       iph_tos;        // Type of service
+    unsigned short int  iph_len;        // IP Packet length (data + header)
+    unsigned short int  iph_ident;      // Identification
+    unsigned short int  iph_flag : 3,   // Fragmentation flags        
+                        iph_offset : 13;// Flags offset
+    unsigned char       iph_ttl;        // Time to Live 
+    unsigned char       iph_protocol;   // Protocol type
+    unsigned short int  iph_chksum;     // IP datagram checksum
+    struct in_addr      iph_sourceip;   // Source IP address
+    struct in_addr      iph_destip;     // Destination IP address
 };
-
-struct ethheader
+/* ICMP Header  */
+struct icmpheader
 {
-    u_char ether_dhost[6];
-    u_char ether_shost[6];
-    u_short ether_type;
+    unsigned char icmp_type;        // ICMP message type
+    unsigned char icmp_code;        // Error code
+    unsigned short int icmp_chksum; // Checksum for ICMP Header and data
+    unsigned short int icmp_id;     // Used for identifying request
+    unsigned short int icmp_seq;    // Sequence number
+    unsigned short int time;
 };
